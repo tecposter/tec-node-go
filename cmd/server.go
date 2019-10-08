af59693400abbc10793a858dcf04a5a8dadbc25a
Anthony Zhan <v-azhan@expedia.com>
2019-10-08 18:15:38 +0800

    integrate with http

--- cmd/server.go
+++ cmd/server.go
@@ -4,12 +4,26 @@ import (
 	"flag"
 	"github.com/tecposter/tec-node-go/lib/iotool"
 	"log"
+	"net/http"
 	"path"
 )
 
 const (
 	defaultBindAddr = "127.0.0.1:7890"
 )
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "static/home.html")
+}
 
 func main() {
 	currDir, err := iotool.CurrDir()
@@ -22,4 +36,11 @@ func main() {
 	flag.Parse()
 
 	log.Printf("data directory: %s, binding addr: %s", *dataDir, *bindAddr)
+
+	http.HandleFunc("/", serveHome)
+
+	err = http.ListenAndServe(*bindAddr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
