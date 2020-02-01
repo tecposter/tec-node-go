e01ca5165c8c8a92d7affdd7e849d68e0cfa799f
Anthony Zhan <v-azhan@expedia.com>
2020-02-01 21:18:31 +0800

    draft.multiSave

--- src/draft/handle.go
+++ src/draft/handle.go
@@ -1,23 +1,27 @@
 package draft
 
 import (
+	// "encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/tecposter/tec-node-go/src/ws"
 )
 
 const (
-	cmdSave   = "draft.save"
-	cmdFetch  = "draft.fetch"
-	cmdList   = "draft.list"
-	cmdDelete = "draft.delete"
-	cmdHas    = "draft.has"
+	cmdSave      = "draft.save"
+	cmdMultiSave = "draft.multiSave"
+	cmdFetch     = "draft.fetch"
+	cmdList      = "draft.list"
+	cmdDelete    = "draft.delete"
+	cmdHas       = "draft.has"
 )
 
 var (
 	errCmdNotFound    = errors.New("Command not found in draft module")
 	errRequirePostID  = errors.New("Require post id")
 	errRequireContent = errors.New("Require content")
+	errRequireDrafts  = errors.New("Require drafts")
 )
 
 // Handle handle ws connection
@@ -25,6 +29,8 @@ func Handle(c *ws.Connection) {
 	switch c.Req().CMD() {
 	case cmdSave:
 		save(c)
+	case cmdMultiSave:
+		multiSave(c)
 	case cmdFetch:
 		fetch(c)
 	case cmdList:
@@ -56,6 +62,39 @@ func save(c *ws.Connection) {
 	err := newServ(c).save(postIDBase58.(string), content.(string))
 	if err != nil {
 		res.SetErr(err)
+	}
+}
+
+func multiSave(c *ws.Connection) {
+	req := c.Req()
+	res := c.Res()
+
+	raw, ok := req.Param("drafts")
+	if !ok {
+		res.SetErr(errRequireDrafts)
+		return
+	}
+
+	rawDrafts := raw.([]interface{})
+	serv := newServ(c)
+	for _, rawDraft := range rawDrafts {
+		draft := rawDraft.(map[string]interface{})
+
+		postID, ok := draft["postID"]
+		if !ok {
+			res.SetErr(errors.New("postID not found"))
+			return
+		}
+		content, ok := draft["content"]
+		if !ok {
+			res.SetErr(errors.New("content not found"))
+			return
+		}
+		err := serv.save(postID.(string), content.(string))
+		if err != nil {
+			res.SetErr(err)
+			return
+		}
 	}
 }
 
